6b9ab91ca0e913ef4d39b1ec27a93b86ac3ae96b
David Edwards <david@more.fool.me.uk>
2013-01-01 15:51:19 +0000

    Fixed location and text for git and apt-get

--- shell/main.sh
+++ shell/main.sh
@@ -5,10 +5,13 @@ PUPPET_DIR=/etc/puppet/
 
 # NB: librarian-puppet might need git installed. If it is not already installed
 # in your basebox, this will manually install it at this point using apt or yum
-if [! -x /usr/bin/git]; then
-    if [ -x /usr/sbin/yum]; then
+GIT=/usr/bin/git
+APT_GET=/usr/bin/apt-get
+YUM=/usr/sbin/yum
+if [ ! -x $GIT ]; then
+    if [ -x $YUM ]; then
         yum -q -y install git
-    elif [ -x /usr/sbin/apt-get ]
+    elif [ -x $APT_GET ]; then
         apt-get -q -y install git
     else
         echo "No package installer available. You may need to install git manually."
