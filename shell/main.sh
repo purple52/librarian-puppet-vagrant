4043de102263ff3d09d60f339ace964ad06fa334
Marc Huffnagle <marc@element84.com>
2013-05-28 16:41:43 -0400

    Made detection of git, yum, and apt-get more dynamic.

--- shell/main.sh
+++ shell/main.sh
@@ -5,24 +5,34 @@ PUPPET_DIR=/etc/puppet/
 
 # NB: librarian-puppet might need git installed. If it is not already installed
 # in your basebox, this will manually install it at this point using apt or yum
-GIT=/usr/bin/git
-APT_GET=/usr/bin/apt-get
-YUM=/usr/sbin/yum
-if [ ! -x $GIT ]; then
-    if [ -x $YUM ]; then
-        yum -q -y makecache
-        yum -q -y install git
-    elif [ -x $APT_GET ]; then
-        apt-get -q -y update
-        apt-get -q -y install git
-    else
-        echo "No package installer available. You may need to install git manually."
-    fi
+
+$(which git > /dev/null 2>&1)
+FOUND_GIT=$?
+if [ "$FOUND_GIT" -ne '0' ]; then
+  echo 'Attempting to install git.'
+  $(which apt-get > /dev/null 2>&1)
+  FOUND_APT=$?
+  $(which yum > /dev/null 2>&1)
+  FOUND_YUM=$?
+
+  if [ "${FOUND_YUM}" -eq '0' ]; then
+    yum -q -y makecache
+    yum -q -y install git
+    echo 'git installed.'
+  elif [ "${FOUND_APT}" -eq '0' ]; then
+    apt-get -q -y update
+    apt-get -q -y install git
+    echo 'git installed.'
+  else
+    echo 'No package installer available. You may need to install git manually.'
+  fi
+else
+  echo 'git found.'
 fi
 
 cp /vagrant/puppet/Puppetfile $PUPPET_DIR
 
-if [ `gem query --local | grep librarian-puppet | wc -l` -eq 0 ]; then
+if [ "$(gem search -i librarian-puppet)" = "false" ]; then
   gem install librarian-puppet
   cd $PUPPET_DIR && librarian-puppet install --clean
 else
