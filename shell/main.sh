29d3a7a80bc1283d0303991410ef2d8eefaed228
David Edwards <david@more.fool.me.uk>
2014-10-19 16:12:48 +0100

    Use librarian-puppet <2.0.0 for Ruby 1.8 fixes #10
    Check the version of Ruby being used. If using 1.8.x, use the 1.x.x series
    of librarian-puppet. Also install Ruby JSON from a package is possible to
    avoid having to install build dependenices to build the native gem.

--- shell/main.sh
+++ shell/main.sh
@@ -1,33 +1,77 @@
 #!/bin/sh
 
+PATH=$PATH:/usr/local/bin/
+
 # Directory in which librarian-puppet should manage its modules directory
 PUPPET_DIR=/etc/puppet/
 
-# NB: librarian-puppet might need git installed. If it is not already installed
-# in your basebox, this will manually install it at this point using apt or yum
-
 $(which git > /dev/null 2>&1)
 FOUND_GIT=$?
-if [ "$FOUND_GIT" -ne '0' ]; then
-  echo 'Attempting to install git.'
-  $(which apt-get > /dev/null 2>&1)
-  FOUND_APT=$?
-  $(which yum > /dev/null 2>&1)
-  FOUND_YUM=$?
+$(which librarian-puppet > /dev/null 2>&1)
+FOUND_LP=$?
+$(which apt-get > /dev/null 2>&1)
+FOUND_APT=$?
+$(which yum > /dev/null 2>&1)
+FOUND_YUM=$?
 
-  if [ "${FOUND_YUM}" -eq '0' ]; then
+InstallLibrarianPuppetGem () {
+  echo 'Attempting to install librarian-puppet gem.'
+  RUBY_VERSION=$(ruby -e 'print RUBY_VERSION')
+  case "$RUBY_VERSION" in
+    1.8.*)
+      # Install the most recent 1.x.x version, but not 2.x.x which needs Ruby 1.9
+      gem install librarian-puppet --version "~>1"
+      ;;
+    *)
+      gem install librarian-puppet
+      ;;
+  esac
+  echo 'Librarian-puppet gem installed.'
+}
+
+if [ "${FOUND_YUM}" -eq '0' ]; then
+
+  # Make sure Git is installed
+  if [ "$FOUND_GIT" -ne '0' ]; then
+    echo 'Attempting to install Git.'
     yum -q -y makecache
     yum -q -y install git
-    echo 'git installed.'
-  elif [ "${FOUND_APT}" -eq '0' ]; then
-    apt-get -q -y update
-    apt-get -q -y install git
-    echo 'git installed.'
-  else
-    echo 'No package installer available. You may need to install git manually.'
+    echo 'Git installed.'
   fi
+
+  # Make sure librarian-puppet is installed
+  if [ "$FOUND_LP" -ne '0' ]; then
+    InstallLibrarianPuppetGem
+  fi
+
+elif [ "${FOUND_APT}" -eq '0' ]; then
+
+  apt-get -q -y update
+
+  # Make sure Git is installed
+  if [ "$FOUND_GIT" -ne '0' ]; then
+    echo 'Attempting to install Git.'
+    apt-get -q -y install git
+    echo 'Git installed.'
+  fi
+
+  # Make sure librarian-puppet is installed
+  if [ "$FOUND_LP" -ne '0' ]; then
+
+    if [ -n "$(apt-cache search librarian-puppet)" ]; then
+       apt-get -q -y install librarian-puppet
+       echo 'Librarian-puppet installed from package'
+    else
+       if [ -n "$(apt-cache search ruby-json)" ]; then
+         # Try and install json dependency from package if possible
+         apt-get -q -y install ruby-json
+       fi
+       InstallLibrarianPuppetGem
+    fi
+  fi
+
 else
-  echo 'git found.'
+  echo 'No supported package installer available. You may need to install git and librarian-puppet manually.'
 fi
 
 if [ ! -d "$PUPPET_DIR" ]; then
@@ -35,10 +79,5 @@ if [ ! -d "$PUPPET_DIR" ]; then
 fi
 cp /vagrant/puppet/Puppetfile $PUPPET_DIR
 
-if [ "$(gem list -i '^librarian-puppet$')" = "false" ]; then
-  gem install librarian-puppet
-  cd $PUPPET_DIR && librarian-puppet install --clean
-else
-  cd $PUPPET_DIR && librarian-puppet update
-fi
+cd $PUPPET_DIR && librarian-puppet install
 
